afd3c1ddb0f8aacf924c5152885042b7010139e0
RafaelFRoque <cupuacu@DESKTOP-E1TT0RR.localdomain>
2020-05-11 16:12:25 -0300

    Method 1 and 3 finalized

--- ex2.py
+++ ex2.py
@@ -2,14 +2,12 @@
 #   Rafael Farias Roque 10295412    
 #   SCC251 
 #   Assignment 2
-#   github.
+#   https://github.com/Oliver-Becker/SCC0251---Assignment2
 
 import numpy as np
 import imageio
-import matplotlib.pyplot as plt
 
 def padding(img, size):
-    
     a = int((size-1)/2)
     m,n = img.shape
     
@@ -49,8 +47,39 @@ def sq_error(img, img_f):
     error = np.sqrt(np.sum(np.power(np.subtract(img_f, img), 2)))
     return error
 
-#def bilateral_filter():
-
+def gaussian_kernel(x, sig):
+    g = float((np.exp((x*x)/(-2*sig*sig)))/(2*np.pi* np.power(sig,2)))
+    return g
+    
+def bilateral_filter(img, n, sig_s, sig_r):
+    img_p = padding(img, n)
+    
+    M, N = img_p.shape
+    a = int((n-1)/2)
+    
+    g_spatial = np.zeros((n,n))
+    
+    for x in range(0, n):
+        for y in range(0, n):
+            g_spatial[x][y] = gaussian_kernel(np.sqrt((x-a)*(x-a) + (y-a)*(y-a)), sig_s)
+            
+            
+    new_img = np.zeros((M,N))
+            
+    for x in range (a, M-a):
+        for y in range(a, N-a):
+            g_range = np.zeros((n,n))
+            for x2 in range(0, n):
+                for y2 in range(0, n):
+                    g_range[x2][y2] =  gaussian_kernel(img_p[x+(x2-a)][y+(y2-a)] - img_p[x][y], sig_r)
+            wi = np.multiply(g_range, g_spatial)
+            Wp = np.sum(wi)
+            If = np.sum(np.multiply(wi,img_p[x-a:x+a+1, y-a:y+a+1]))
+            new_img[x][y] = If/Wp
+            
+    new_img = unpadding(new_img, n)
+    return new_img
+    
 def laplacian_filter(img, c, kernel):
         
     img_p = padding(img, 3)
@@ -70,10 +99,29 @@ def laplacian_filter(img, c, kernel):
     
     return img_lf
 
-#def vignette_filter():
+def vignette_filter(img, sig_row, sig_col):
+    M,N = img.shape
+    a = np.ceil((M/2)-1)
+    b = np.ceil((N/2)-1)
+    g_row = np.zeros((1,M))
+    g_col = np.zeros((1,N))
+    for x in range(0, M):
+        g_row[0][x] = gaussian_kernel(x-a, sig_row)
+    for x in range(0, N):
+        g_col[0][x] = gaussian_kernel(x-b, sig_col)
     
+    g_row = g_row.T
+    
+    g_filter = np.matmul(g_row, g_col)
+
+    img_vf = np.multiply(g_filter, img)
+    
+    img_vf = normalize(img_vf)
+
+    return img_vf
+
 # reading the input
-filename = str(input())
+filename = str(input()).rstrip()
 img = imageio.imread(filename)
 img = img.astype(np.float64)
 method = int(input())
@@ -83,8 +131,8 @@ if method == 1:
     n =  int(input())
     sig_s = float(input())
     sig_r = float(input())
-    #new_img = bilateral_filter()
-    #print(sq_error(img, new_img))
+    new_img = bilateral_filter(img, n, sig_s, sig_r)
+    print(sq_error(img, new_img))
     if S == 1:
         imageio.imwrite(filename, new_img.astype(np.uint8))
 elif method == 2:
@@ -98,8 +146,8 @@ elif method == 2:
 elif method == 3:
     sig_row = float(input())
     sig_col = float(input())
-    #new_img = vignette_filter()
-    #print(sq_error(img, new_img))
+    new_img = vignette_filter(img, sig_row, sig_col)
+    print(sq_error(img, new_img))
     if S == 1:
         imageio.imwrite(filename, new_img.astype(np.uint8))
     
